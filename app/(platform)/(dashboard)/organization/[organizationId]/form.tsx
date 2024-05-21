64e6c5bd87d6496773e125aa60395bd2268fbd59
Ryota Kojima <ryotakojima@RyotanoMacBook-Pro.local>
2024-05-21 23:41:33 +0900

    formコンポーネントの共通化

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import { createBoard } from '@/actions/create-board'
-import { FormInput } from './form-input'
-import { FormButton } from './form-button'
+
 import { useAction } from '@/hooks/use-action'
+import { FormInput } from '@/components/form/form-input'
+import { FormSubmit } from '@/components/form/form-submit'
 
 export const Form = () => {
   const { execute, fieldErrors } = useAction(createBoard, {
@@ -17,6 +18,8 @@ export const Form = () => {
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get('title') as string
+
+    console.log({ title })
     execute({ title })
   }
 
@@ -27,9 +30,9 @@ export const Form = () => {
   return (
     <form action={onSubmit} className="flex items-center gap-x-2">
       <div className="flex flex-col space-y-2">
-        <FormInput errors={fieldErrors} />
+        <FormInput label="Board Title" id="title" errors={fieldErrors} />
       </div>
-      <FormButton />
+      <FormSubmit>Savea</FormSubmit>
     </form>
   )
 }
